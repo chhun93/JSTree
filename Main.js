80ee522c845e75ff435b2905028c6d274681b72e
chhun93 <chhun3830@gmail.com>
2021-01-11 22:31:49 +0900

    Set

--- Main.js
+++ Main.js
@@ -1,5 +1,6 @@
 const http = require("http");
 const url = require("url");
+const qs = require("querystring");
 
 const template = (body) => {
   return `
@@ -9,16 +10,51 @@ const template = (body) => {
                 <title>JS TREE</title>
             </head>
             <body>
+                <h1 id="homeName"> JS TREE </h1>
                 ${body}
-            </body>
-        </html>        
-    `;
-};
 
+                <style>${GetStyle()}</style>
+            </body>
+        </html>
+         `;
+};
+const GetStyle = () => {
+  return `
+    #homeName { text-align: center; }
+    #createTextBox { width : 25rem; height: 2rem; }
+`;
+};
+const CreateTextBox = () => {
+  return `
+  <p><input id="createTextBox" type="text" placeholder="CONTENT"> </p>
+`;
+};
+const CreateButton = () => {
+  return `
+
+`;
+};
+const UpdateButton = () => {
+  return `
+
+`;
+};
+const DeleteButton = () => {
+  return `
+
+`;
+};
 const app = http.createServer((req, res) => {
-  var html = template(`<h1> HELLO </h1>`);
-  res.writeHead(200);
-  res.end(html);
+  const reqUrl = req.url;
+  const queryData = url.parse(reqUrl, true).query;
+  const pathName = url.parse(reqUrl, true).pathname;
+
+  if (pathName === "/") {
+    var html = template(CreateTextBox());
+
+    res.writeHead(200);
+    res.end(html);
+  }
 });
 
 app.listen(3000);
