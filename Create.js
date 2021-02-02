29775ea261d56e309cbf1c63b600275ae67dbbad
chhun93 <chhun3830@gmail.com>
2021-02-02 21:09:37 +0900

    너무 복잡해;;

--- Create.js
+++ Create.js
@@ -9,5 +9,7 @@ const ClickCreateText = (inputDiv) => {
   } else {
     document.getElementById("_input_confirm").value = "CREATE";
     inputDiv.style.visibility = "visible";
+    document.getElementById("_input_title").value = "";
+    document.getElementById("_input_content").value = "";
   }
 };
